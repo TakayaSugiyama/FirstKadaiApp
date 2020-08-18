ee7a1e692618900be95c2a84d105656e78e064da
takayasugiyama <s.takaya1027@gmail.com>
2020-08-19 04:53:23 +0900

    Initialize Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 杉山貴哉 on 2020/08/19.
+//  Copyright © 2020 TakayaSugiyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
